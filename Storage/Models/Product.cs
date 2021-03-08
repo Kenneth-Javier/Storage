1d46d431dee455a0eab12cf868998d30cbaffbce
Kenneth-Javier <kj.ortigoza@gmail.com>
2021-03-08 09:26:07 +0100

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Range(0,int.MaxValue)]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public int Price { get; set; }
+
+        [Display(Name ="Order Date")]
+        DateTime DateTime { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Category { get; set; }
+
+        [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "-")]
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+
+        [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "-")]
+        public string Description { get; set; }
+        
+    }
+}
